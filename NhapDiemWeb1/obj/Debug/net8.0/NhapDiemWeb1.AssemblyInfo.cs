6b7294bac3e4ec49fe679c5eeffe782c913ff9d9
tokhoi <tokhoi1312@gmail.com>
2026-01-19 14:42:59 +0700

    syncmanagement-path1

--- NhapDiemWeb1/obj/Debug/net8.0/NhapDiemWeb1.AssemblyInfo.cs
+++ NhapDiemWeb1/obj/Debug/net8.0/NhapDiemWeb1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NhapDiemWeb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4fe7c1aa099695d3a222f356b2c543a67ab530")]
 [assembly: System.Reflection.AssemblyProductAttribute("NhapDiemWeb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NhapDiemWeb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
